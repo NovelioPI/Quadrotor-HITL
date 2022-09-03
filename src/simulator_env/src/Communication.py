61f65a0af7a74cd60a84e1bcd65eecea8e96e944
NovelioPI <novelio.p.i@mail.ugm.ac.id>
2022-09-03 07:02:41 +0700

    add sensors thread for real time communication

--- src/simulator_env/src/Communication.py
+++ src/simulator_env/src/Communication.py
@@ -13,24 +13,50 @@ from threading import Thread
 
 class Communication:
     def __init__(self):
-        self._imu_data = None
-        self._alt_data = None
-        self._gps_data = None
+        self._imu_data = Imu()
+        self._alt_data = Range()
+        self._gps_data = NavSatFix()
 
-    def run_node(self, port='/dev/ttyUSB0', baud=57600, rate=10):
-        self._init_serial(port, baud)
-        self._init_thread()
-
+    def run_node(self, port='/dev/ttyUSB0', baud=57600, rate=100):
         rospy.init_node('Communication', anonymous=True)
         self.rate = rospy.Rate(rate)
 
-    def _get_data(self):
-            try:
-                self._imu_data = rospy.wait_for_message('/raw_imu', Imu, timeout=1)
-                self._alt_data = rospy.wait_for_message('/sonar_height', Range, timeout=1)
-                self._gps_data = rospy.wait_for_message('/fix', NavSatFix, timeout=1)
+        self._init_serial(port, baud)
+        self._init_thread()
 
-                self.data = "{:.8f},{:.8f},{:.8f},{:.8f},{:.8f},{:.8f},{:.8f},{:.8f},{:.8f},{:.8f}\n".format(
+    def _update_imu(self):
+        while not rospy.is_shutdown():
+            try:
+                self._imu_data = rospy.wait_for_message('/raw_imu', Imu, timeout=1)    
+
+            except:
+                pass
+                
+        self.rate.sleep()
+
+    def _update_alt(self):
+        while not rospy.is_shutdown():
+            try:
+                self._alt_data = rospy.wait_for_message('/sonar_height', Range, timeout=1)
+                
+            except:
+                pass
+                
+        self.rate.sleep()
+    
+    def _update_gps(self):
+        while not rospy.is_shutdown():
+            try:
+                self._gps_data = rospy.wait_for_message('/fix', NavSatFix, timeout=1)
+                
+            except:
+                pass
+                
+        self.rate.sleep()
+
+    def _get_data(self):
+        while not rospy.is_shutdown():
+            self._data = "{:.8f},{:.8f},{:.8f},{:.8f},{:.8f},{:.8f},{:.8f},{:.8f},{:.8f},{:.8f}\n".format(
                     self._imu_data.angular_velocity.x,
                     self._imu_data.angular_velocity.y,
                     self._imu_data.angular_velocity.z,
@@ -41,47 +67,69 @@ class Communication:
                     self._gps_data.latitude,
                     self._gps_data.longitude,
                     self._gps_data.altitude)
-            except:
-                pass
+            self.rate.sleep()
     
     def _mcu_read(self):
         while not rospy.is_shutdown():
+            start = perf_counter()
             try:
-                data = self._ser.readline()
-                print(data.decode(), end="")
-            except:
-                pass
-
-    def _mcu_write(self):
-        while not rospy.is_shutdown():
-            try:
-                self._get_data()
-
                 if not self._ser.open:
                     self._init_serial()
                 else:
-                    self._ser.write(self.data.encode())
+                    data = self._ser.readline()
+                    print(data.decode(), end="")
             except:
                 pass
-            
             self.rate.sleep()
+            # print("read:",perf_counter() - start)
+        else:
+            return
+
+    def _mcu_write(self):
+        while not rospy.is_shutdown():
+            start = perf_counter()
+            try:
+                if not self._ser.open:
+                    self._init_serial()
+                else:
+                    self._ser.write(self._data.encode())
+            except:
+                pass
+            self.rate.sleep()
+            # print("write:",perf_counter() - start)
+        else:
+            return
 
     def _init_serial(self, port, baud):
         self._ser = Serial(port, baud, timeout=1)
         try:
             self._ser.open()
+            self._ser.flush()
         except:
             pass
     
     def _init_thread(self):
+        imu_th = Thread(target=self._update_imu)
+        alt_th = Thread(target=self._update_alt)
+        gps_th = Thread(target=self._update_gps)
+        data_th = Thread(target=self._get_data)
         transmit_th = Thread(target=self._mcu_write)
         receive_th = Thread(target=self._mcu_read)
         
         try:
+            imu_th.start()
+            alt_th.start()
+            gps_th.start()
+            data_th.start()
             transmit_th.start()
             receive_th.start()
         except:
             pass
+    
+    def _shutdown(self):
+        self._ser.cancel_read()
+        self._ser.cancel_write()
+        self._ser.close()
 
     @property
     def imu(self):
@@ -99,4 +147,4 @@ class Communication:
 if __name__ == '__main__':
     com = Communication()
 
-    com.run_node(port='/dev/ttyACM0', rate=50)
+    com.run_node(port='/dev/ttyACM0', baud=115200,rate=100)
